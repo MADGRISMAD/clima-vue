52a2703c5f20c832b8e3e21c99b2e0e4fcc3e0f9
madeline <madgrismad@gmail.com>
2024-04-01 04:04:44 -0700

    Eliminar componentes y estilos no utilizados

---
+++ src/components/Formulario.vue
@@ -0,0 +1,33 @@
+<script setup>
+const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+
+</script>
+<template>
+    <form class="formulario
+    ">
+    <div class="campo">
+        <label for="ciudad">ciudad</label>
+        <input 
+        type="text" 
+        id="ciudad" 
+        v-model="ciudad" 
+        required>
+
+    </div>
+    <div class="pais">
+        <label for="pais">País</label>
+        <select value="">--seleccione un pais--</select>
+
+    </div>
+    
+    </form>
+</template>
+
